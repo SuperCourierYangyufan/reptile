d3d8fa6b6b3e62ffdcbff11b6ae9bbc7da0318b7
e <l>
2020-12-07 16:15:11 +0800

    init

---
+++ src/test/java/com/my/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
